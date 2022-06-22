ff44a195e4cc5e01617dfb31e5731e9d7e87e02f
Rokut <101550057+Rokut@users.noreply.github.com>
2022-06-22 09:54:46 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,104 @@
+import { useEffect, useState} from 'react';
+import './App.css';
+import { Header } from './components/Header';
+import { SearchFilter } from './components/SearchFilter';
+import { CountryContainer } from './components/CountryContainer';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Country } from './components/Country';
+import axios from 'axios';
+
+function App() {
+  // States
+  const [region, setRegion] = useState("Filter by Region");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [countriesData, setCountriesData] = useState([]);
+  const [url, setUrl] = useState("https://restcountries.com/v3.1/all");
+
+  // paimti duomenys
+  const fetchHandler = async (url) => {
+    try{
+        let res = await axios.get(url);
+        if(res.status === 404){
+            setError('Countries not found');
+            setLoading(false);
+        }else{
+            setCountriesData(res.data);
+            setLoading(false);
+            setError(false);
+        }
+    }
+    catch(error){
+        setLoading(false);
+        setError(error.message);
+    }
+  }
+
+  // call the function handler once every render
+  useEffect(() => {
+    fetchHandler(url);
+     // eslint-disable-next-line
+  }, []); 
+
+  // call the function handler when the url is changed
+  useEffect(() => {
+    fetchHandler(url);
+     // eslint-disable-next-line
+  }, [url]);
+  
+  // Submit Handler
+  const submitHandler = (e) => {
+    if(e.length > 3){
+      setUrl(`https://restcountries.com/v3.1/name/${e}`);
+    }
+    if(e.length === 0){
+      setUrl("https://restcountries.com/v3.1/all");
+    }
+    setRegion('Filter by region');
+  }
+
+  // Regiono pasirinkimo uzsetinimas
+  const regionHandler = (r) => {
+    setRegion(r);
+    if(r && r !== 'All'){
+      setUrl(`https://restcountries.com/v3.1/region/${r}`);
+    }else{
+      setRegion("Filter by region");
+      setUrl("https://restcountries.com/v3.1/all");
+    }
+  }
+
+  // Gaut salies varda
+  const getCountryName = (code) => {
+    return countriesData.filter((element) => {
+      return element.cca3 === code;
+    }).map((el, i) => el.name.common)
+  }
+
+  // Pagrindinio puslapio komponentas
+  const HomePage = () => {
+    return (
+      <>
+        <SearchFilter region = {region} setRegion = {regionHandler} submitHandler = {submitHandler}/>
+        { error ? <div className="font-bold text-2xl">{error}</div> : 
+          loading && !countriesData.error ? <div className="font-bold text-2xl">Loading...</div> :  <CountryContainer data={countriesData}/> }
+      </>
+    )
+  }
+
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <main>
+          <Routes>
+            <Route exact path='/rest-countries/' element={ <HomePage /> }></Route>
+            <Route exact path='/rest-countries/:countryName/' element= { <Country getCountryName = {getCountryName} /> }></Route>
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+export const Header = () => {
+    return (
+        <header>
+            <h1 className="text-[1.4rem]">Countries app api</h1>
+        </header>
+    );
+}
